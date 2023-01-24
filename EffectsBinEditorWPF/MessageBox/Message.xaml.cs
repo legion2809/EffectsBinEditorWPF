ec463b5597849b64e98a3e15d3b87406e4e6114d
Shatilov Ivan <boss.vanek12@mail.ru>
2023-01-24 19:36:25 +0300

    Little improvement at custom message box

--- EffectsBinEditorWPF/MessageBox/Message.xaml.cs
+++ EffectsBinEditorWPF/MessageBox/Message.xaml.cs
@@ -9,7 +9,8 @@ public partial class Message : Window
 
     public void SetFields(string TextHeader, string Text, byte a, byte r, byte g, byte b)
     {
-        Title.Content = TextHeader;
+        Title = TextHeader;
+        TitleText.Content = TextHeader;
         Description.Content = Text;
         this.Background = new SolidColorBrush(Color.FromArgb(a, r, g, b));
     }
